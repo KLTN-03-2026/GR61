70f2bf41f0f250606f7551b4b47d41402b5fd08a
Trân Huy Hoàng <tranhuyhoangg7903@gmail.com>
2026-05-16 16:31:36 +0700

    Fix giao dien, phat trien chuc nang todolistêê

--- app/users/todo/page.tsx
+++ app/users/todo/page.tsx
@@ -9,7 +9,7 @@ export default function TodoPage() {
       <aside className="h-full bg-white shadow-sm scale-90 border-r-2 border-slate-50">
         <WeeklyCalendar />
       </aside>
-      {/* Nội dung danh sách nhiệm vụ .*/}
+      {/* Nội dung danh sách nhiệm vụ.*/}
       <section className="flex-1 h-full overflow-y-auto bg-white scale-95 no-scrollbar">
         <h1 className="text-6xl text-center text-black font-black uppercase italic tracking-tighter mt-4">
           TODOLIST
